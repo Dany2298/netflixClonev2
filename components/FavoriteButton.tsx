bae2d466a130e71a9b62025c8b7a1d636d9404ca
Dany2298 <dany22986@gmail.com>
2023-07-27 20:48:22 +0100

    update

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -11,42 +11,44 @@ interface FavoriteButtonProps {
 }
 
 const FavoriteButton: React.FC<FavoriteButtonProps> = ({movieId}) => {
-    const {mutate: mutateFavorites} = useFavorites();
+    // const {mutate: mutateFavorites} = useFavorites();
 
-    const {data: currentUser, mutate} = useCurrentUser();
+    // const {data: currentUser, mutate} = useCurrentUser();
 
-    const isFavorite = useMemo(() => {
-        const list = currentUser?.favoriteIds || []; //search in our current user, in their favorite id's
+    // const isFavorite = useMemo(() => {
+    //     const list = currentUser?.favoriteIds || []; //search in our current user, in their favorite id's
 
-        return list.includes(movieId); //if the current list includes the passed in movie id.
-    },[currentUser, movieId]);
+    //     return list.includes(movieId); //if the current list includes the passed in movie id.
+    // },[currentUser, movieId]);
 
-    const toggleFavorites = useCallback(async()  => {
-        let response;
+    // const toggleFavorites = useCallback(async()  => {
+    //     let response;
 
-        if (isFavorite) { //check if current movie is favorited
-            response = await axios.delete('/api/favorite', {data: {movieId}}); // if it is, we trigger axios delete
-        } else {
-            response = await axios.post('/api/favorite', {movieId}); 
-        }
+    //     if (isFavorite) { //check if current movie is favorited
+    //         response = await axios.delete('/api/favorite', {data: {movieId}}); // if it is, we trigger axios delete
+    //         console.error(response);
+    //     } else {
+    //         response = await axios.post('/api/favorite', {movieId}); 
+    //         console.error(response);
+    //     }
 
-        const updatedFavoriteIds = response?.data?.favoriteIds;
+    //     const updatedFavoriteIds = response?.data?.favoriteIds;
 
-        mutate({
-            ...currentUser,
-            favoriteIds: updatedFavoriteIds
-        });
+    //     mutate({
+    //         ...currentUser,
+    //         favoriteIds: updatedFavoriteIds
+    //     });
 
-        mutateFavorites();
-    },[movieId, isFavorite, currentUser, mutate, mutateFavorites])
+    //     mutateFavorites();
+    // },[movieId, isFavorite, currentUser, mutate, mutateFavorites])
 
-    const Icon = isFavorite ? AiOutlineCheck : AiOutlinePlus;
+    // const Icon = isFavorite ? AiOutlineCheck : AiOutlinePlus;
 
 
     return (
-        <div onClick={toggleFavorites} 
+        <div onClick={() => {}} 
          className='cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300'>
-            <Icon className='text-white' size={25}/>
+            < AiOutlinePlus className='text-white cursor-no-drop' size={25}/>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import Billboard from '../components/Billboard';
 import MovieList from '../components/MovieList';
 import useMovieList from '../hooks/UseMovieList';
 import useFavorites from '../hooks/useFavorites';
+import InfoModal from '../components/InfoModal';
+import useInfoModal from '../hooks/useInfoModal';
 
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -28,16 +30,18 @@ export async function getServerSideProps(context: NextPageContext) {
 export default function Home(){
 const { data:movies = [] } = useMovieList(); //using movie list from our hook
 const {data: favorites = []} = useFavorites();
+const {isOpen, closeModal } = useInfoModal();
 
 
 
   return (
     <>
+    <InfoModal visible={isOpen} onClose={closeModal}/>
     <Navbar />
     <Billboard />
     <div className='pb-40'>
     <MovieList title="Trending Now" data={movies}  />
-    <MovieList title="My List" data={favorites}  />
+    <MovieList title="My List" data={movies}  />
     </div>
     </>
   )
